d133b166329b61889d19065f644f76dc142458d4
Hameul0v0 <sky990612@gmail.com>
2024-07-09 19:22:55 +0900

    SKYtimes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = `0c12040e60d948238f7f4c43e1b7b55f`;
 let news=[];
 
 const getLatestNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
     console.log("uuu", url);
 
     const response = await fetch(url);
