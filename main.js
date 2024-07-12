7d0b617cc0629d0c0a0a42267043f754515db070
Haneul0v0 <sky990612@gmail.com>
2024-07-12 19:34:15 +0900

    SKYtimes

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const API_KEY = `0c12040e60d948238f7f4c43e1b7b55f`;
 let newsList = [];
 
-// let url = new URL(
-// `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}` )
+// let url = new
+// URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
 let url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us`
 );
 
-const menus = document.querySelectorAll(".menus button");
-menus.forEach(
-    menu => menu.addEventListener("click", (event) => getNewsByCategory(event))
-);
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
 
 const getNews = async () => {
     try {
+        url
+            .searchParams
+            .set("page", page);
+        url
+            .searchParams
+            .set("pageSize", pageSize);
+
         const response = await fetch(url);
         const data = await response.json();
 
@@ -23,7 +30,9 @@ const getNews = async () => {
             }
 
             newsList = data.articles;
+            totalResults = data.totalResults;
             render();
+            paginationRender();
         } else {
             throw new Error(data.message);
         }
@@ -37,7 +46,6 @@ const getLatestNews = async () => {
     // `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}` );
     url = new URL(
         `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us`
-
     );
 
     await getNews();
@@ -63,13 +71,19 @@ const getNewsByKeyword = async () => {
         .getElementById("search-input")
         .value;
 
+    if (keyword.trim() === "") 
+        return;
+    
     // url = new URL(
     // `https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&apiKey=${API_KEY}`
-    // )
+    // );
     url = new URL(
         `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&q=${keyword}`
+    );
 
-    )
+    document
+        .getElementById("search-input")
+        .value = "";
 
     await getNews();
 }
@@ -136,7 +150,66 @@ const errorRender = (errorMessage) => {
         .innerHTML = errorHTML;
 }
 
-document.addEventListener("DOMContentLoaded", getLatestNews);
+const paginationRender = () => {
+    let paginationHTML = ``;
+    let totalPages = Math.ceil(totalResults / pageSize);
+    let pageGroup = Math.ceil(page / groupSize);
+    let lastPage = pageGroup * groupSize
+    let firstPage = lastPage - (groupSize - 1) <= 0
+        ? 1
+        : lastPage - (groupSize - 1);
+
+    if (lastPage > totalPages) {
+        lastPage = totalPages;
+    }
+
+    if (firstPage >= 6) {
+        paginationHTML = `
+            <li class="page-item" onclick="moveToPage(1)">
+                <a class="page-link">&lt;&lt;</a>
+            </li>
+            <li class="page-item" onclick="moveToPage(${page - 1})">
+                <a class="page-link">&lt;</a>
+            </li>`;
+    }
+
+    for (let i = firstPage; i <= lastPage; i++) {
+        paginationHTML += `
+            <li class="page-item ${i == page
+            ? "active"
+            : ""}" onclick="moveToPage(${i})">
+                <a class="page-link">${i}</a>
+            </li>`;
+    }
+
+    if (lastPage < totalPages) {
+        paginationHTML += `
+        <li class="page-item" onclick="moveToPage(${page + 1})">
+            <a class="page-link">&gt;</a>
+        </li>
+        <li class="page-item" onclick="moveToPage(${totalPages})">
+            <a class="page-link" href='#js-bottom'>&gt;&gt;</a>
+        </li>`;
+    }
+
+    document
+        .querySelector(".pagination")
+        .innerHTML = paginationHTML
+
+}
+
+const moveToPage = (pageNum) => {
+    console.log("moveToPage", pageNum);
+    page = pageNum;
+    getNews();
+}
+
+getLatestNews();
+
+const menus = document.querySelectorAll(".menus button");
+menus.forEach(
+    menu => menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
 
 document.addEventListener('DOMContentLoaded', function () {
     let menuIcon = document.getElementById('menu-icon');
